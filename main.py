e5f564946f596620b52117312a8c7fbf82b49abc
DodoMels <107034626+DodoMels@users.noreply.github.com>
2023-01-09 18:50:57 +0100

    änderung an code von branch 2

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
     # branch 1 is herer
-
-
+dsgffdg
+fhfgh
+hjkfhk
+nkgkjh
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
