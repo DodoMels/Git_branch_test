b259fc194deabf3ce28687b450207f419b13207b
DodoMels <107034626+DodoMels@users.noreply.github.com>
2023-01-09 18:24:05 +0100

    branch 1 is herer

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
+    # branch 1 is herer
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
