52f8868e0137f1214dc035dd4e73dcacfadbe969
DodoMels <107034626+DodoMels@users.noreply.github.com>
2023-01-09 18:46:23 +0100

    gepusht

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def print_hi(name):
     änderunenen kommen
 
     ich schreibe weiter und weiter
-
+djfjkdg
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
